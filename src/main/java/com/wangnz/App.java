f8c98bedb9e9f5a15636497a70032a95f971d34a
wangnz <nanzhi@eastmoney.com>
2018-03-29 15:50:48 +0800

    add

--- src/main/java/com/wangnz/App.java
+++ src/main/java/com/wangnz/App.java
@@ -1,15 +1,16 @@
 package com.wangnz;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Hello world!
  */
 public class App {
 
-    //    private static Logger logger = LoggerFactory.getLogger(App.class);
-    private static Logger logger = LogManager.getLogger(App.class);
+        private static Logger logger = LoggerFactory.getLogger(App.class);
+//    private static Logger logger = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
         logger.info("Hello World!");
